69f1b80144c1ff821722df95bc068f9ef7536871
Gareth_Mellon <garethmellon@gmail.com>
2019-02-27 14:38:02 +0000

    added user reg

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLogin
+from accounts.forms import UserLogin, UserRegistrationForm
 
 # Create your views here.
 
@@ -35,3 +35,27 @@ def login(request):
     else:
         login_form = UserLogin()
     return render(request, "login.html", {'login_form': login_form})
+    
+def registration(request):
+    """ render the registration page"""
+    if request.user.is_authenticated():
+        return redirect(reverse('index'))
+        
+    if request.method=="POST":
+        registration_form = UserRegistrationForm(request.POST)
+        
+        if registration_form.is_valid():
+            registration_form.save()
+            
+            user = auth.authenticate(username = request.POST['username'],
+                                        password = request.POST['password1'])
+        
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have seccessfully registered.")
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "Unable to register your account at this time.")
+            
+    registration_form = UserRegistrationForm()
+    return render(request, "registration.html", { 'registration_form': registration_form })
